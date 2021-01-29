7be47b06ecb2e198ce078d82cfe5d160e9eb4133
Danielle Leppert-Simenauer <danielle.leppertsimenauer@gmail.com>
2021-01-29 22:48:18 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,21 @@
+    def greeting
+      puts "Hello World"
+    end
+    
+    def say_greeting_five_times
+      greeting
+      greeting
+      greeting
+      greeting
+      greeting
+    end
+    
+    say_greeting_five_times
+    
+    def stylish_chef
+      best_hairstyle = "Guy Fieri"
+      return "Martha Stewart"
+      "Guy Fieri"
+    end
+    
+    stylish_chef
